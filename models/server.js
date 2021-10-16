0bfd984756576405c9604528277142a2363f8347
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-16 16:50:42 -0300

    server structure added

---
+++ models/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+
+
+
+class Server{
+
+   constructor(){
+    this.app = express();
+    this.port = process.env.PORT || 8080;
+    this.path ={
+      //path-e:"endpoint"
+    };
+
+    //base de datos
+
+    //middlewares
+
+    //rutas de mi app
+
+
+   }
+
+
+   middlewares(){
+    //cors
+    this.app.use(cors());
+
+    //lectura y parseo del body
+    this.app.use(express.json());
+
+    //directorio publico
+    this.app.use(express.static("public"));
+
+   }
+
+
+   routes(){
+    // this.app.use(this.path.path-e, require("route"));
+   }
+
+   listen(){
+    this.app.listen(this.port);
+   }
+
+}
+
+module.exports = Server;
