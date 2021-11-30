4d8f3337acf711683940f61b824c5ec480f91c39
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-11-30 18:57:01 -0300

    login configurated passowrd , state and exist

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,45 @@
 const { request, response } = require("express");
+const bcryptjs = require("bcryptjs");
+const User = require("../models/user");
+
+const login = async(req = request, res = response) => {
+
+  const {email, password} = req.body;
+
+  try {
+    
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({
+         msg:"Usuario/contraseña incorrectos"
+      });
+    }
+
+    if(user.state === false ){
+      return res.status(400).json({
+         msg:"Usuario/contraseña incorrectos"
+      })
+    }
+  
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if(!validPassword){
+      return res.status(400).json({
+        msg:"Usuario/contraseña incorrectos"
+      });
+    }
+    
+    res.status(200).json({
+      msg:"login ok"
+    })
 
 
-const login = (req = request, res = response) => {
 
-  res.json({
-    msg:"login"
-  })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+       msg:"Hable con el administrador"
+    }) 
+  }
 
 }
 
